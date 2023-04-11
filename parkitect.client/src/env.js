9c184c74be600177c35048f9ae81c2888681b85b
Joseph Bowers <jsphbowers@gmail.com>
2023-04-11 16:50:57 -0600

    Initial setup

--- parkitect.client/src/env.js
+++ parkitect.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-b4j0gsbo788hk3il.us.auth0.com'
+export const clientId = '1KAZn34pSIV1HGCIL7KlDJen3ff604pQ'
+export const audience = 'http://sandbox.io'
