b42add1a0b66d0ec99be3080be5ca4c569f78f3e
alexmquan <alexmquan93@gmail.com>
2023-04-12 10:26:22 -0600

    Completed Trip Schema

--- parkitect/server/db/DbContext.js
+++ parkitect/server/db/DbContext.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { TripThingToDoSchema } from "../models/TripThingToDo.js";
+import { TripSchema } from "../models/Trip.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 
-  TripThingToDo = mongoose.model('TripThingToDo', TripThingToDoSchema)
+  TripThingToDo = mongoose.model('TripThingToDo', TripThingToDoSchema);
+  Trip = mongoose.model('Trip', TripSchema);
 }
 
 export const dbContext = new DbContext()
---
+++ parkitect/server/models/Trip.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+export const TripSchema = new Schema({
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
+  name: { type: String, required: true, minLength: 0, maxLength: 200 },
+  description: { type: String, maxLength: 1000 },
+  coverImg: { type: String, maxLength: 1000, },
+  joinCode: { type: String, required: true, minLength: 6, maxLength: 6 }
+})
