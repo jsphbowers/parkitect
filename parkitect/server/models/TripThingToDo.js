421b2d971d302c974ef50afbc46a402001361578
alexmquan <alexmquan93@gmail.com>
2023-04-12 10:19:27 -0600

    Built TripThingToDo Schema

---
+++ parkitect/server/models/TripThingToDo.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+export const TripThingToDoSchema = new Schema({
+  nativeThingToDoId: { type: String, required: true, maxLength: 500 },
+  tripId: { type: Schema.Types.ObjectId, required: true, ref: "Trip" },
+  title: { type: String, required: true, minLength: 0, maxLength: 100 },
+  activities: { type: Array, max: 500 }
+})
