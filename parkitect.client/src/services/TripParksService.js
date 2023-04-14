d2c86a89d84c7f2b1aacb9c59cf9bfc90ff431db
Joseph Bowers <jsphbowers@gmail.com>
2023-04-14 15:37:19 -0600

    Working on dropdown in park details

--- parkitect.client/src/services/TripParksService.js
+++ parkitect.client/src/services/TripParksService.js
@@ -4,7 +4,7 @@ import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class TripParksService {
-  async getTripParkByTripId(tripId) {
+  async getTripParksByTripId(tripId) {
     const res = await api.get(`trips/${tripId}/tripParks`)
     logger.log('[GETTING TRIPPARKS]', res.data)
     AppState.tripParks = res.data.map(tp => new TripPark(tp))
