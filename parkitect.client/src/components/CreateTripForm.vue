e316a643557043365500b1eb8dcfadb9f714099f
AnastasiiaShaynyuk <anashaynyuk@gmail.com>
2023-04-14 11:25:29 -0600

    added image preview

--- parkitect.client/src/components/CreateTripForm.vue
+++ parkitect.client/src/components/CreateTripForm.vue
@@ -1,53 +1,85 @@
 <template>
   <form @submit.prevent="createTrip()">
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="name" placeholder="Name" name="name" required v-model="editable.name">
+      <input
+        type="text"
+        class="form-control"
+        id="name"
+        placeholder="Name"
+        name="name"
+        required
+        v-model="editable.name"
+      />
       <label for="name">Name</label>
     </div>
     <div class="form-floating mb-3">
-      <input type="url" class="form-control" id="mgUrl" placeholder="ImgUrl" name="imgUrl" required
-        v-model="editable.coverImg">
+      <input
+        type="url"
+        class="form-control"
+        id="mgUrl"
+        placeholder="ImgUrl"
+        name="imgUrl"
+        required
+        v-model="editable.coverImg"
+        @input="previewImage"
+      />
       <label for="imgUrl">ImgUrl</label>
+      <br>
+      <img :src="imagePreview" v-if="imagePreview" style="max-width: 100%" />
     </div>
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="description" placeholder="Description" name="description" required
-        v-model="editable.description">
+      <textarea
+        class="form-control"
+        id="description"
+        placeholder="Description"
+        name="description"
+        required
+        style="height: 100px"
+        v-model="editable.description"
+      ></textarea>
       <label for="description">Description</label>
     </div>
     <div class="my-3 text-end">
-      <button data-bs-dismiss="modal" class="btn btn-success" type="submit">Create!</button>
+      <button data-bs-dismiss="modal" class="btn btn-success" type="submit">
+        Create!
+      </button>
     </div>
   </form>
 </template>
-
 
 <script>
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { logger } from "../utils/Logger";
 import Pop from "../utils/Pop";
-import {tripsService} from '../services/TripsService.js'
+import { tripsService } from "../services/TripsService.js";
 
 export default {
   setup() {
-    const router = useRouter()
-    const editable = ref({})
+    const router = useRouter();
+    const editable = ref({});
+    const imagePreview = ref(null);
+
+    
     return {
       editable,
+      imagePreview,
       async createTrip() {
-        try { 
-          const tripData = editable.value
-          const trip = await tripsService.createTrip(tripData)
-          router.push({ name: "TripDetails", params: {tripId:  trip.id } })
+        try {
+          const tripData = editable.value;
+          const trip = await tripsService.createTrip(tripData);
+          router.push({ name: "TripDetails", params: { tripId: trip.id } });
         } catch (error) {
-          logger.error(error)
-          Pop.toast(error.message, 'error')
+          logger.error(error);
+          Pop.toast(error.message, "error");
         }
-      }
+      },
+      previewImage() {
+      imagePreview.value = editable.value.coverImg;
     }
-  }
-}
+    };
+  },
+};
 </script>
-
 
 <style lang="scss" scoped></style>
