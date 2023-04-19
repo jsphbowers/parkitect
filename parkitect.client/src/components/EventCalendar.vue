bf9404fedc986d9d8ce964820ca21f1b3973e3d6
alexmquan <alexmquan93@gmail.com>
2023-04-19 16:32:09 -0600

    Calendar Add

---
+++ parkitect.client/src/components/EventCalendar.vue
@@ -0,0 +1,28 @@
+<template>
+  <FullCalendar />
+</template>
+
+
+<script>
+import '@fullcalendar/core/vdom'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin
+
+export default {
+
+  setup() {
+    components: {
+      FullCalendar // make the <FullCalendar> tag available
+    }
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth'
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
