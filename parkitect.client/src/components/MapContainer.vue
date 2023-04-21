48c60c012a7c689d0a0bfb59439926e03894e3b4
Elizabeth Keyes <liz4momentum@gmail.com>
2023-04-21 10:19:32 -0600

    Add InfoWindow to each map Marker

--- parkitect.client/src/components/MapContainer.vue
+++ parkitect.client/src/components/MapContainer.vue
@@ -2,24 +2,37 @@
   <GoogleMap api-key="AIzaSyATZoG7a3Rf97UDfWBnc9wFsuEB5PKkh7Q" style="width: 100%; height: 500px" :center="center"
     :zoom="4">
     <MarkerCluster>
-      <Marker v-if="locations" v-for="location in locations" :key="location" :options="{ position: location }" />
+      <Marker v-if="locations" v-for="(location, i) in locations" :key="i" :options="{ position: location }">
+
+        <InfoWindow>
+          <div id="content">
+            <div id="siteNotice"></div>
+            <h6 id="firstHeading" class="firstHeading">{{ parks[i].name }}</h6>
+            <!-- <div id="bodyContent">
+              <p>{{ parks[i].description }}</p>
+            </div> -->
+          </div>
+        </InfoWindow>
+
+      </Marker>
     </MarkerCluster>
   </GoogleMap>
 </template>
 
 <script>
 import { computed, defineComponent } from 'vue'
-import { GoogleMap, Marker, MarkerCluster } from 'vue3-google-map'
+import { GoogleMap, Marker, MarkerCluster, InfoWindow } from 'vue3-google-map'
 import { AppState } from "../AppState.js"
 
 export default defineComponent({
-  components: { GoogleMap, Marker, MarkerCluster },
+  components: { GoogleMap, Marker, MarkerCluster, InfoWindow },
   setup() {
     const center = { lat: 39.1000, lng: -96.8167 }
 
     return {
       center,
-      locations: computed(() => AppState.locations)
+      locations: computed(() => AppState.locations),
+      parks: computed(() => AppState.parks)
 
     }
   },
